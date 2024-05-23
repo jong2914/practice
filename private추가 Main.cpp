ad67fada947c5c89a5145770cd0733dde9028b27
jong2914 <163897786+jong2914@users.noreply.github.com>
2024-05-23 22:32:43 +0900

    Create private추가 Main.cpp

---
+++ private추가 Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#include "CarS3.h"
+#include "SUVS3.h"
+int main() {
+	SUV* newSUV = new SUV();
+	newSUV->setPrice(5000);
+	cout << " price " << newSUV->getPrice() << "\n";
+
+	newSUV->setmodelYear(2022);
+	cout << "Model year: " << newSUV->getmodelYear() << endl;
+	cout << "Model year: " << newSUV->modelYear << endl;
+
+	cout << "Fuel efficiency: " << newSUV->fuelefficiency << endl;
+	delete newSUV;
+	return 0;
+}
