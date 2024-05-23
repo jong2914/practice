c4374a14b3184a70f0bf1cd7e8727e08ab30f179
jong2914 <163897786+jong2914@users.noreply.github.com>
2024-05-23 11:04:27 +0900

    Create C9-1Main.cpp

---
+++ C9-1Main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include"ani.h"
+#include"hum.h"
+#include"bird.h"
+#include"fish.h"
+using namespace std;
+int main() {
+	animal a;
+	human b;
+	bird c;
+	fish d;
+	a.eat();
+	b.eat();
+	c.eat();
+	d.eat();
+	b.walk();
+	c.fly();
+	d.swim();
+	cout << " b " << b.nLegs << endl;
+	return 123;
+	
+}
