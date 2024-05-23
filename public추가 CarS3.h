9223d49d2983d82746be1503899ce0a9cb2d2161
jong2914 <163897786+jong2914@users.noreply.github.com>
2024-05-23 22:06:55 +0900

    Create public추가 CarS3.h

---
+++ public추가 CarS3.h
@@ -0,0 +1,26 @@
+#pragma once
+class Car {
+public:
+	Car() :speed(0.1), wheels(0), price(0.2), modelYear(2020) {};
+	Car(float, int, float);
+	void setPrice(float);
+	float getPrice();
+
+	void setmodelYear(int year);
+	int getmodelYear();
+
+	float speed;
+	void speedChange(float);
+	int wheels;
+	void setWheels(int);
+	int getWheels();
+	void klaxon(int);
+protected:
+	float price;
+	float maxspeed;
+	void speedUp(float);
+	void speedDown(float);
+public:
+	int modelYear;
+
+};
